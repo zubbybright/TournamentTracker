44da1ea36c3ba24216864a10019619627eabd69d
Zubby Uwajimgba <zubbybrightson@gmail.com>
2022-10-31 15:51:07 +0100

    additional classes

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -5,12 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TrackerLibrary
-{
+{   
+    /// <summary>
+    /// Represents one person
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// The first name of the person
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The cell phone number of the person
+        /// </summary>
         public string CellphoneNumber { get; set; }
+
+        /// <summary>
+        /// The email of the person
+        /// </summary>
         public string EmailAddress { get; set; }
 
 
